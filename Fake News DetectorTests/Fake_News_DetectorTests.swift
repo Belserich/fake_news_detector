20a22616a78716d0f5964d44a235ea468800d0b1
Ingenious-App-Studios <ingenious-app-studios.co.uk>
2019-05-18 16:24:18 +0200

    Initial Commit

---
+++ Fake News DetectorTests/Fake_News_DetectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fake_News_DetectorTests.swift
+//  Fake News DetectorTests
+//
+//  Created by Adedayo Omosanya on 18/05/2019.
+//  Copyright © 2019 Adedayo Omosanya. All rights reserved.
+//
+
+import XCTest
+@testable import Fake_News_Detector
+
+class Fake_News_DetectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
